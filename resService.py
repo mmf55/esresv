9930038676b20e0cf86864bdbfb7711a776b7777
mfernandes <manuelfernandes55@ua.pt>
2015-11-29 16:04:58 +0000

    More webservices added

--- resService.py
+++ resService.py
@@ -1,6 +1,7 @@
 import time
+from icalendar import Calendar, Event, vDatetime
 from datetime import datetime, timedelta
-from flask import Flask, json, request
+from flask import Flask, json, request, make_response
 from flask_restful import Api, Resource
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists
@@ -25,6 +26,9 @@ api = Api(app)
 
 
 class Reservations(db.Model):
+    """
+        Defines the database table using SQLAlchemy for storing the reservations.
+    """
     __tablename__ = "reservations"
     reservationID = db.Column(db.Integer, primary_key=True)
     itemID = db.Column(db.Integer, db.ForeignKey('stock.itemID'))
@@ -50,6 +54,9 @@ class Reservations(db.Model):
 
 
 class Stock(db.Model):
+    """
+        Defines the database table using SQLAlchemy for storing the stock available.
+    """
     __tablename__ = "stock"
     itemID = db.Column(db.Integer, primary_key=True)
     providerID = db.Column(db.Integer)
@@ -78,6 +85,29 @@ class Stock(db.Model):
 
 
 class AllReservations(Resource):
+    """
+        [GET] Returns a set of all reservations on the service in a JSON format.
+        JSON sent:
+        {
+            "allreservations": {
+                "0": [
+                    {
+                        "clientID": 9,
+                        "quantity": 3,
+                        "timestamp": 1448733600
+                    }
+                ],
+                "1": [
+                    {
+                        "clientID": 9,
+                        "quantity": 1,
+                        "timestamp": 1448913600
+                    }
+                ]
+            }
+        }
+    """
+
     def get(self):
         if database_exists(DATABASE) is False:
             db.create_all()
@@ -97,6 +127,18 @@ class AllReservations(Resource):
 
 
 class DoReservation(Resource):
+    """
+        [POST] Set a new reservation on the service and returns the ID assigned to that reservations.
+        JSON received:
+        {
+            "itemID": 8,
+            "quantity": 2,
+            "clientID": 12,
+            "username": "dave1",
+            "timestamp": 1445556339
+        }
+    """
+
     def post(self):
         if database_exists(DATABASE) is False:
             db.create_all()
@@ -105,7 +147,7 @@ class DoReservation(Resource):
         if stock is None:
             return "401 Invalid itemID"
         if stock.stockQuantity < in_data['quantity']:
-            return "402 Available stock is less than the requested value"
+            return "402 Invalid stock"
         res = Reservations(stock.itemID,
                            in_data['quantity'],
                            in_data['clientID'],
@@ -129,13 +171,99 @@ class DoReservation(Resource):
                         )
         dh.add_event(reservation.reservationID, stock.itemName, in_data['timestamp'])
 
+        return {"reservationID": reservation.reservationID}
+
+    def get(self):
+        return "400 Invalid Operation"
+
+
+class UpdateReservation(Resource):
+    """
+        [POST] Updates the info related to a reservation. Only updates the parameters received that is nonzero.
+        JSON received:
+        {
+            "quantity": 2,
+            "timestamp": 1445556339
+        }
+    """
+    def post(self, reservation_id):
+        if database_exists(DATABASE) is False:
+            db.create_all()
+        in_data = request.get_json(force=True)
+        if in_data['quantity'] < 0 or in_data['timestamp'] < 0:
+            return "401 Invalid parameter value"
+        reservation = Reservations.query.filter_by(reservationID=reservation_id).first()
+        if reservation is None:
+            return "402 Reservation ID is not valid"
+        if in_data['quantity'] > 0:
+            stock = Stock.query.filter_by(itemID=reservation.itemID).first()
+            if in_data['quantity'] > reservation.quantity:
+                more = in_data['quantity'] - reservation.quantity
+                if more <= stock.stockQuantity:
+                    reservation.quantity += more
+                    stock.stockQuantity -= more
+                else:
+                    return "403 Invalid stock"
+            elif in_data['quantity'] < reservation.quantity:
+                less = reservation.quantity - in_data['quantity']
+                reservation.quantity -= less
+                stock.stockQuantity += less
+
+        if in_data['timestamp'] > 0:
+            reservation.timestamp = in_data['timestamp']
+        db.session.commit()
         return "200 OK"
 
     def get(self):
         return "400 Invalid Operation"
 
 
+class DeleteReservation(Resource):
+    """
+        [GET] Deletes a reservation with a given reservation ID.
+    """
+    def get(self, reservation_id):
+        reservation = Reservations.query.filter_by(reservationID=reservation_id).first()
+        if reservation is None:
+            return "401 Reservation ID is not valid"
+        stock = Stock.query.filter_by(itemID=reservation.itemID).first()
+        stock.stockQuantity += reservation.quantity
+        db.session.delete(reservation)
+        db.session.commit()
+        return "200 OK"
+
+    def post(self):
+        return "400 Invalid Operation"
+
+
 class ReplenishStock(Resource):
+    """
+        [POST] Inserts new stock in the database.
+        JSON received:
+        {
+            "info": [
+                {
+                    "username": "dave1",
+                    "providerID": 1
+                }
+            ],
+            "menu": [
+                {
+                    "itemID": 8,
+                    "price": 10,
+                    "name": "peixe",
+                    "quantity": 20
+                },
+                {
+                    "itemID": 9,
+                    "price": 10,
+                    "name": "carne",
+                    "quantity": 20
+                }
+            ]
+        }
+    """
+
     def post(self):
         if database_exists(DATABASE) is False:
             db.create_all()
@@ -162,6 +290,25 @@ class ReplenishStock(Resource):
 
 
 class AllStock(Resource):
+    """
+        [GET] Returns all the stock available on the service.
+        JSON sent:
+        {
+            "allstock": [
+                {
+                    "itemID": 0,
+                    "itemName": "pratodeteste",
+                    "itemQuantity": 12
+                },
+                {
+                    "itemID": 1,
+                    "itemName": "pratodeteste222",
+                    "itemQuantity": 17
+                }
+            ]
+        }
+    """
+
     def get(self):
         if database_exists(DATABASE) is False:
             db.create_all()
@@ -177,6 +324,29 @@ class AllStock(Resource):
 
 
 class UserReservations(Resource):
+    """
+        [GET] Returns all the reservations made by a specific user.
+        JSON sent:
+        {
+            "reservations": [
+                {
+                    "itemID": 8,
+                    "itemName": "peixe",
+                    "timestamp": 123452356343,
+                    "provider": "dave1",
+                    "quantity": 2
+                },
+                {
+                    "itemID": 8,
+                    "itemName": "peixe",
+                    "timestamp": 123452356343,
+                    "provider": "dave1",
+                    "quantity": 2
+                }
+            ]
+        }
+    """
+
     def get(self, username):
         if database_exists(DATABASE) is False:
             db.create_all()
@@ -194,11 +364,30 @@ class UserReservations(Resource):
 
 
 class ProviderStock(Resource):
-    def get(self, provider_name):
+    """
+        [GET] Returns all the stock available from one provider.
+        JSON sent:
+        {
+            "stock": [
+                {
+                    "itemID": 8,
+                    "itemName": "peixe",
+                    "itemQuantity": 18
+                },
+                {
+                    "itemID": 9,
+                    "itemName": "carne",
+                    "itemQuantity": 20
+                }
+            ]
+        }
+    """
+
+    def get(self, provider_id):
         if database_exists(DATABASE) is False:
             db.create_all()
         l2 = []
-        stock = Stock.query.filter_by(provider_name=provider_name).all()
+        stock = Stock.query.filter_by(providerID=provider_id).all()
         l = [a.to_json() for a in stock]
         for item in l:
             l2.append({'itemID': item['itemID'], 'itemName': item['itemName'], 'itemQuantity': item['stockQuantity']})
@@ -209,17 +398,33 @@ class ProviderStock(Resource):
 
 
 class ProviderReservated(Resource):
-    def get(self, provider_name):
+    """
+        [GET] Returns all the future reservations related to a particular provider.
+        JSON sent:
+        {
+            "reservated": [
+                {
+                    "itemID": 9,
+                    "itemName": "carne",
+                    "quantity": 2
+                }
+            ]
+        }
+    """
+
+    def get(self, provider_id):
         if database_exists(DATABASE) is False:
             db.create_all()
-        stock_provider = Stock.query.filter_by(provider_name=provider_name).all()
+        stock_provider = Stock.query.filter_by(providerID=provider_id).all()
         l2 = []
+        timestamp_now = int(time.time())
         for item in stock_provider:
             reserv = Reservations.query.filter_by(itemID=item.itemID).all()
             if len(reserv) != 0:
                 count = 0
                 for item2 in reserv:
-                    count += item2.quantity
+                    if item2.timestamp > timestamp_now:
+                        count += item2.quantity
                 l2.append({'itemID': item.itemID, 'itemName': item.itemName, 'quantity': count})
         return {"reservated": l2}
 
@@ -228,6 +433,18 @@ class ProviderReservated(Resource):
 
 
 class ProviderReservatedTF(Resource):
+    """
+        [GET] Returns the total reservation that one provider has in the future.
+        JSON sent:
+        {
+            "reservated": [
+                {
+                    "total": 0
+                }
+            ]
+        }
+    """
+
     def get(self, provider_id):
         if database_exists(DATABASE) is False:
             db.create_all()
@@ -247,35 +464,109 @@ class ProviderReservatedTF(Resource):
 
 
 class ProviderReservatedTFDay(Resource):
-    def get(self, provider_id):
+    """
+        [POST] Returns all the reservations from one provider for one specific day.
+        JSON received:
+        {
+            "date": "26/11/2015"
+        }
+
+        JSON sent:
+        {
+            "reservated" : [
+            {
+                "itemID": 1,
+                "reservations" : [
+                    {
+                        "itemID": 0,
+                        "itemName": "dsf",
+                        "quantity": 2,
+                        "timestamp": 123342512343,
+                        "username": "mmf"
+                    },
+                    {
+                        "itemID": 0,
+                        "itemName": "dsf",
+                        "quantity": 2,
+                        "timestamp": 12334251233,
+                        "username": "mmf"
+                    }
+                ]
+            },
+            {
+                "itemID": 2,
+                "reservations" : [
+                    {
+                        "itemID": 0,
+                        "itemName": "dsf",
+                        "quantity": 2,
+                        "timestamp": 123342512343,
+                        "username": "mmf"
+                    },
+                    {
+                        "itemID": 0,
+                        "itemName": "dsf",
+                        "quantity": 2,
+                        "timestamp": 12334251233,
+                        "username": "mmf"
+                    }
+                ]
+            }
+
+            ]
+        }
+    """
+
+    def post(self, provider_id):
         if database_exists(DATABASE) is False:
             db.create_all()
         in_data = request.get_json(force=True)
         stock_provider = Stock.query.filter_by(providerID=provider_id).all()
         l2 = []
-        dater = datetime.datetime.strptime(in_data['date'], "%d/%m/%Y")
+        dater = datetime.strptime(in_data['date'], "%d/%m/%Y")
         timestamp_begin = int(time.mktime(dater.timetuple()))
-        timestamp_end = int(time.mktime((dater + datetime.timedelta(1)).timetuple()))
+        timestamp_end = int(time.mktime((dater + timedelta(1)).timetuple()))
         for item in stock_provider:
             reserv = Reservations.query.filter_by(itemID=item.itemID).all()
             if len(reserv) != 0:
+                l3 = []
                 for item2 in reserv:
                     if (item2.timestamp > timestamp_begin) and (item2.timestamp < timestamp_end):
-                        l2.append({'itemID': item.itemID, 'itemName': item.itemName, 'quantity': item2.quantity,
+                        l3.append({'quantity': item2.quantity,
+                                   'timestamp': item2.timestamp,
                                    'username': item2.username})
+                if len(l3) != 0:
+                    l2.append({'itemID': item.itemID, 'itemName': item.itemName, 'reservations': l3})
         return {"reservated": l2}
 
-    def post(self):
+    def get(self):
         return "400 Invalid Operation"
 
 
 class GetCaldavFile(Resource):
+    """
+        [GET] Returns a file related to a reservation in iCal format.
+    """
+
     def get(self, reservation_id):
         if database_exists(DATABASE) is False:
             db.create_all()
         reservation = Reservations.query.filter_by(reservationID=reservation_id).first()
-        return json.dumps(
-            {'url': 'http://ogaviao.ddns.net/%s/meals.ics/%d.ics' % (reservation.username, reservation_id)})
+        stock = Stock.query.filter_by(itemID=reservation.itemID).first()
+
+        cal = Calendar()
+        cal['version'] = "2.0"
+        cal['prodid'] = "//Radicale//NONSGML Radicale Server//EN"
+        event = Event()
+        event['uid'] = reservation.reservationID
+        event['dtstart'] = vDatetime(datetime.fromtimestamp(reservation.timestamp)).to_ical()
+        event['summary'] = stock.itemName
+        event['x-radicale-name'] = str(reservation.reservationID) + '.ics'
+        cal.add_component(event)
+
+        response = make_response(cal.to_ical())
+        response.headers["Content-Disposition"] = "attachment; filename=calendar.ics"
+        return response
 
 
 class CheckStock(Resource):
@@ -293,7 +584,7 @@ class CheckStock(Resource):
 class ResetDatabase(Resource):
     def get(self):
         if database_exists(DATABASE) is False:
-            return "406 Database doesn't exist!"
+            return "401 Database doesn't exist"
         db.drop_all()
         db.create_all()
         return "200 OK"
@@ -301,11 +592,13 @@ class ResetDatabase(Resource):
 
 api.add_resource(AllReservations, '/allreservations')
 api.add_resource(DoReservation, '/doreservation')
+api.add_resource(UpdateReservation, '/updatereserv/<reservation_id>')
+api.add_resource(DeleteReservation, '/deletereserv/<reservation_id>')
 api.add_resource(ReplenishStock, '/replenishstock')
 api.add_resource(AllStock, '/allstock')
 api.add_resource(UserReservations, '/userresv/<username>')
-api.add_resource(ProviderStock, '/providerstock/<provider_name>')
-api.add_resource(ProviderReservated, '/reservationsnumber/<provider_name>')
+api.add_resource(ProviderStock, '/providerstock/<provider_id>')
+api.add_resource(ProviderReservated, '/reservationsnumber/<provider_id>')
 api.add_resource(ProviderReservatedTF, '/reservtotal/<provider_id>')
 api.add_resource(ProviderReservatedTFDay, '/dayreserv/<provider_id>')
 api.add_resource(GetCaldavFile, '/getfile/<reservation_id>')
